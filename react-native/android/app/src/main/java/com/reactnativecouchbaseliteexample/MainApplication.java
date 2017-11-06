76047e09a13463fa6de5fdbb7184711b951cb5bd
Tommy Le <tommy.le2@gmail.com>
2017-11-06 16:02:30 -0500

    Fixed build issues

--- react-native/android/app/src/main/java/com/reactnativecouchbaseliteexample/MainApplication.java
+++ react-native/android/app/src/main/java/com/reactnativecouchbaseliteexample/MainApplication.java
@@ -1,12 +1,14 @@
 package com.reactnativecouchbaseliteexample;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+<<<<<<< ours
 import me.fraserxu.rncouchbaselite.ReactCBLiteManager;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
+=======
+>>>>>>> theirs
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -19,7 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
@@ -30,6 +32,11 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactCBLiteManager(),
             new VectorIconsPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
